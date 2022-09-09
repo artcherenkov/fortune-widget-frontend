2832e116bb1fb323b4ff0194cfca4e0f6491f55b
Артем Черенков <art.cherenkov@gmail.com>
2022-09-09 12:50:45 +0700

    Добавлена возможность повторного запуска колеса, валидация формы

--- examples/script.js
+++ examples/script.js
@@ -54,23 +54,22 @@ const onWidgetScriptLoad = () => {
 
 onWidgetScriptLoad();
 
-// const onWheelScriptLoad = () => {
-//   console.log("wheel script loaded");
-//   const widgetScript = document.createElement("script");
-//   widgetScript.type = "text/javascript";
-//   widgetScript.defer = true;
-//   // widgetScript.src = "https://unpkg.com/fortune-widget@1.0.0/dist/index.js";
-//   wheelScript.src = "../dist/index.js";
-//
-//   widgetScript.onload = onWidgetScriptLoad;
-//
-//   document.head.appendChild(widgetScript);
-// };
-//
-// const wheelScript = document.createElement("script");
-// wheelScript.type = "text/javascript";
-// wheelScript.defer = true;
-// wheelScript.src = "https://unpkg.com/fortune-wheel@2.0.10/dist/index.js";
-// wheelScript.onload = onWheelScriptLoad;
-//
-// document.head.appendChild(wheelScript);
+const onWheelScriptLoad = () => {
+  console.log("wheel script loaded");
+  const widgetScript = document.createElement("script");
+  widgetScript.type = "text/javascript";
+  widgetScript.defer = true;
+  widgetScript.src = "https://unpkg.com/fortune-widget@1.0.1/dist/index.js";
+
+  widgetScript.onload = onWidgetScriptLoad;
+
+  document.head.appendChild(widgetScript);
+};
+
+const wheelScript = document.createElement("script");
+wheelScript.type = "text/javascript";
+wheelScript.defer = true;
+wheelScript.src = "https://unpkg.com/fortune-wheel@2.0.11/dist/index.js";
+wheelScript.onload = onWheelScriptLoad;
+
+document.head.appendChild(wheelScript);
